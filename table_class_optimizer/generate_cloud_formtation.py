dd39568c135563c9c117638f33ea90e48e847f5e
Max Rabin <max.rabin@mobileye.com>
2024-11-28 14:40:22 +0200

    Start generating cloudformation stack with code to implement the recommendations of the optimizer query

---
+++ table_class_optimizer/generate_cloud_formtation.py
@@ -0,0 +1,41 @@
+from textwrap import indent
+
+
+def my_indenter(num: int, text: str) -> str:
+    return indent(text, " " * num)[num:]
+
+
+def replace_query_parameters(query: str) -> str:
+    return (
+        query.replace(
+            "ARRAY['ALL'] AS account_ids", "ARRAY[${AccountIds}] AS account_ids"
+        )
+        .replace("ARRAY['ALL'] AS payer_ids", "ARRAY[${PayerIds}] AS payer_ids")
+        .replace("ARRAY['ALL'] AS table_names", "ARRAY[${TableNames}] AS table_names")
+        .replace(
+            "ARRAY['ALL'] AS region_names", "ARRAY[${RegionNames}] AS region_names"
+        )
+    )
+
+
+def main():
+    with open("./DDB_TableClassReco.sql", "r") as sql, open(
+        "./lambda_handler.py", "r"
+    ) as lambda_handler, open("./stack_template.yaml", "r") as stack_template, open(
+        "./stack_template_output.yaml", "w"
+    ) as output:
+        result: str = (
+            stack_template.read()
+            .replace(
+                "{{ athena_query_string }}",
+                my_indenter(10, replace_query_parameters(sql.read())),
+            )
+            .replace(
+                "{{ lambda_handler_code }}", my_indenter(10, lambda_handler.read())
+            )
+        )
+        output.write(result)
+
+
+if __name__ == "__main__":
+    main()
